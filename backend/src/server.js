2827e5a2f255a2fc79f9d5ac86e00bb85830d626
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2024-10-21 23:23:58 +0000

    database setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import routeNotFound from './middleware/not-found.js';
 import questions from './routes/questions.js';
 import topics from './routes/topics.js';
+import connectDB from './db/connect.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 3010;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 
@@ -17,6 +19,15 @@ app.use('/api/v1/topics', topics);
 
 app.use(routeNotFound);
 
-app.listen(PORT, () => {
-  console.log(`API started and running at address http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`API started and running at address http://localhost:${PORT} and successfuly connected to database`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
